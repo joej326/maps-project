a9aabb60f5286b9d458cdea518d086dcb45bd0a3
Joseph Joyce <joej326@gmail.com>
2018-12-08 15:06:35 -0500

    starting on title screen comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { TitleScreenComponent } from './title-screen/title-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    TitleScreenComponent
   ],
   imports: [
     BrowserModule,
